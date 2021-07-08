e5c503f67c89fd1e9e805359af273e8ec72e76ea
Caê <eduardokae01@hotmail.com>
2021-07-08 20:35:38 -0300

    requisito 1 - shopping cart

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-//   return section;
-// }
+  return section;
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -40,4 +40,23 @@
 //   return li;
 // }
 
-// window.onload = () => { };
+const addItens = (itens) => {
+  itens.forEach((item) => {
+    const createItem = createProductItemElement(item);
+    const section = document.querySelector('.items');
+    section.appendChild(createItem);
+  });
+};
+
+const getItensML = (product) => { //  Requisito 1 resolvido com ajuda de Jackson Pires - Professor turma 13 A
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+    .then((response) => {
+      response.json().then((data) => {
+        addItens(data.results);
+      });
+    });
+};
+
+window.onload = () => {
+  getItensML('computador');
+};
