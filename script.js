291b24be8e36238732b10009d72b406aebd054b7
Lanai Conceição <conceicao.lanai@gmail.com>
2021-07-08 19:57:57 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+// const fetch = require('node-fetch');
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+// ==============================================
 
-//   return section;
-// }
+// Requisito 1
+
+// ==============================================
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
+}
+
+// Função que resgata os elementos do JSON e enviá-los para o html dinamicamente [Requisito 1]
+
+const getJsonOnLink = async (query) => {
+  const api = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=$${query}`);
+  const apiJson = await api.json();
+  const arrayResultsJson = apiJson.results;
+  arrayResultsJson.forEach((product) => document.querySelector('.items')
+    .appendChild(createProductItemElement(product)));
+};
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -40,4 +58,6 @@
 //   return li;
 // }
 
-// window.onload = () => { };
+window.onload = () => { 
+  getJsonOnLink('computador');
+};
