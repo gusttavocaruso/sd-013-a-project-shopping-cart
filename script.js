42cda7e8924526bc83d48b2b7d5a46edf84d4c3f
icaroMelo <exemployt@gmail.com>
2021-07-08 18:24:33 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-//   return section;
-// }
+  return section;
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -40,4 +40,18 @@
 //   return li;
 // }
 
-// window.onload = () => { };
+const r1 = (produto) => {
+const req1 = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
+fetch(req1)
+  .then((rep) => rep.json()
+  .then((prodjson) => {
+    prodjson.results.forEach((item) => {
+      const sectItem = document.querySelectorAll('.items')[0];
+      sectItem.appendChild(createProductItemElement(item));
+    });
+  }));
+};
+
+window.onload = () => {
+  r1('computador');
+ };
