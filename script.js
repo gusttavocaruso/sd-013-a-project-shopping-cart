215cc994fca86672d9cfd99cea99a9a47c55f61b
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-07-08 17:01:45 -0300

    fix lint

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function createProductImageElement(imageSource) {
+/* function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+} */
 
-function createCustomElement(element, className, innerText) {
+/* function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+} */
 
-/*function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -22,23 +22,23 @@ function createCustomElement(element, className, innerText) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-}/*
+}
 
-/*function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}*/
+} */
 
-/*function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
-}*/
+} */
 
-/*function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}*/
+} */
 
 // fonte: https://www.youtube.com/watch?v=m3K8DP4kVXQ
 function productList() {
