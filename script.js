776d78edd9a21b18e0ae30237db2f459780fff57
Juliane Marques <mqs.juliane@gmail.com>
2021-07-08 18:01:44 -0300

    refatorando requisito 1 parte 002

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-
+// const fetch = require('node-fetch');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -37,7 +36,6 @@ const fetchURL = async () => {
   });
 };
 
-
 /* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -46,13 +44,13 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 } */
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
 
 window.onload = function onload() {
   fetchURL();
