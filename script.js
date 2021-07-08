9c6a2b53531d675a4eceb027648f2e6f7080e62f
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-07-08 18:37:14 -0300

    req 2 eu acho

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
- function createProductImageElement(imageSource) {
+const cart = document.querySelector('.cart__items');
+
+function saveItens() {
+  localStorage.setItem('cart', cart.innerHTML);
+}
+
+function getSavedItens() {
+  cart.innerHTML = localStorage.getItem('cart');
+}
+
+function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
@@ -24,21 +34,36 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-/* function getSkuFromProductItem(item) {
+function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-} */
+}
 
-/* function cartItemClickListener(event) {
-  // coloque seu código aqui
-} */
+function cartItemClickListener(event) {
+  event.target.remove();
+  saveItens();
+} 
 
-/* function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
+
+function fillCart() {
+  const item = event.target.parentElement;
+  const itemID = getSkuFromProductItem(item);
+  const url = 'https://api.mercadolibre.com/items/${itemID}';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((object) => {
+      object.forEach((item) => {
+        cart.appendChild(createCartItemElement(item));
+      });
+    });
+}
 
 // fonte: https://www.youtube.com/watch?v=m3K8DP4kVXQ
 function productList() {
@@ -56,4 +81,6 @@ function productList() {
 
 window.onload = () => { 
   productList();
+  getSavedItens();
+  fillCart();
 };
