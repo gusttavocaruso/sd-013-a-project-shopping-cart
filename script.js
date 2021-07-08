d7a8780dcf3dcef6e8814c6e37213010d3c92ee0
fgosuen <fgosuendacosta@gmail.com>
2021-07-08 18:44:50 -0300

    Iniciando projeto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function createProductImageElement(imageSource) { //
+function createProductImageElement(imageSource) { // abc
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
