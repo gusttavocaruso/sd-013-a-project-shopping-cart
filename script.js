42907ea475f8876324be04d62a559248aa7a1a15
VictorDizne <victoralbertocostadiniz@gmail.com>
2021-07-08 17:11:11 -0300

    Completa o projeto mas ainda estou com um requisito quebrado não sei exatamente o porque, vou resolver no plantão assim que abrir

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const productsContainer = document.querySelector('.items');
 const cart = document.querySelector('.cart__items');
 const totalPrice = document.querySelector('.total-price');
+const clearCartButton = document.querySelector('.empty-cart');
+const changeSearchButton = document.querySelector('.change-search');
 
 const addTotalPrice = async (productPrice) => {
   const currentPrice = Number(totalPrice.innerHTML);
-  totalPrice.innerHTML = (currentPrice + productPrice).toFixed(2);
+  totalPrice.innerHTML = Math.round((currentPrice + productPrice) * 100) / 100;
   if (totalPrice.innerHTML === '0.00' || totalPrice.innerHTML === 0.00) {
     totalPrice.innerHTML = '0';
   }
@@ -12,14 +14,16 @@ const addTotalPrice = async (productPrice) => {
 
 const saveLocalStorage = () => {
   localStorage.setItem('cart', cart.innerHTML);
+  localStorage.setItem('total-price', totalPrice.innerHTML);
 };
 
 const loadLocalStorage = () => {
   cart.innerHTML = localStorage.getItem('cart');
+  totalPrice.innerHTML = localStorage.getItem('total-price');
 };
 
-async function getElements() {
-  return fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+async function getElements(searchItem) {
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${searchItem}`)
     .then((r) => r.json())
     .then((r) => r.results);
 }
@@ -96,24 +100,44 @@ const addListenersToProductButtons = () => {
   buttons.forEach((button) => button.addEventListener('click', async (event) => {
     const product = event.target.parentElement;
     const itemID = getSkuFromProductItem(product);
+    cart.appendChild(createCustomElement('h1', 'loading', 'loading...'));
+    const loading = document.querySelector('.loading');
     let infoDoProduto = await fetch(`https://api.mercadolibre.com/items/${itemID}`);
     infoDoProduto = await infoDoProduto.json();
+    cart.removeChild(loading);
     addTotalPrice(infoDoProduto.price);
     cart.appendChild(createCartItemElement(infoDoProduto));
     saveLocalStorage();
   }));
 };
 
-async function addProductsToPage() {
-  const elementos = await getElements();
+async function addProductsToPage(elements) {
+  productsContainer.appendChild(createCustomElement('h1', 'loading', 'loading...'));
+  const elementos = await getElements(elements);
+  productsContainer.innerHTML = '';
   await elementos.forEach((elemento) => {
     productsContainer.appendChild(createProductItemElement(elemento));
   });
   addListenersToProductButtons();
 }
 
+clearCartButton.addEventListener('click', () => {
+  cart.innerHTML = '';
+  totalPrice.innerHTML = 0;
+  saveLocalStorage();
+});
+
+changeSearchButton.addEventListener('click', () => {
+  const newSearchValue = document.querySelector('.input').value;
+  if (newSearchValue === null || newSearchValue === '' || newSearchValue === ' ') {
+    return alert('Insira um item válido');
+  }
+  document.querySelector('.input').value = '';
+  addProductsToPage(newSearchValue);
+});
+
 window.onload = function onload() {
-  addProductsToPage();
+  addProductsToPage('computador');
   loadLocalStorage();
   const selectedProducts = document.querySelectorAll('.cart__item');
   selectedProducts.forEach((product) => product.addEventListener('click', (event) => {
