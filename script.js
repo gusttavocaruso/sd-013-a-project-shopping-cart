d5de1dc31560152fa4215ed6fa32e3d5a00963fd
Japa <wesleyy_@live.com>
2021-07-08 17:03:39 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -24,20 +24,35 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
-window.onload = () => { };
+const fetchAPI = (product) => {
+  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.forEach((item) => {
+        const itemsSection = document.getElementsByClassName('items');
+        console.log(itemsSection);
+        itemsSection[0].appendChild(createProductItemElement(item));
+      });
+    });
+ };
+
+window.onload = () => {
+  fetchAPI('computador');
+};
