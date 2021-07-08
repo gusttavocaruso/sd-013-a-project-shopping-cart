3fa0300b0db380ea2b8aa9fb8751ac932131df27
V. Dionysio <vini.dionysio@gmail.com>
2021-07-08 15:42:26 -0300

    Solving lint errors

--- script.js
+++ script.js
@@ -24,24 +24,24 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 // function cartItemClickListener(event) {
 //   // coloque seu código aqui
 // }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 const getData = async () => {
-  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   const response = await fetch(API_URL);
 
   if (!response.ok) {
