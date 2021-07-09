5f3d175c61b6854359816e2abb64660978ad8ed6
Cássio Pereira <cassiorodp@gmail.com>
2021-07-09 10:44:13 -0300

    Requisito 2 completo

--- script.js
+++ script.js
@@ -30,17 +30,18 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  console.log(event);
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
 // Aux
 const createItems = (results) => {
@@ -51,10 +52,19 @@ const createItems = (results) => {
   });
 };
 
-const eventListener = (elements) => {
-  elements.forEach((element) => {
+const buttonEventListener = () => {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((element) => {
     element.addEventListener(('click'), (event) => {
-      console.log(event.target.parentElement);
+      const itemId = event.target.parentNode.firstChild.innerText;
+      // console.log(itemId);
+      fetch(`https://api.mercadolibre.com/items/${itemId}`)
+        .then((response) => response.json()
+          .then((data) => {
+            const cartItems = document.querySelector('.cart__items');
+            const cartItem = createCartItemElement(data);
+            cartItems.appendChild(cartItem);
+          }));
     });
   });
 };
@@ -64,8 +74,7 @@ const getProductList = () => {
       .then((response) => {
         response.json().then((data) => {
           createItems(data.results);
-          const buttons = document.querySelectorAll('.item__add');
-          eventListener(buttons);
+          buttonEventListener();
         });
       })
       .catch((error) => console.log(error));
