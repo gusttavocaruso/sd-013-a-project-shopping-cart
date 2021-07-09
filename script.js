fca8244fd0dc22601b52f35a2a68fdd9dae6be62
ArthurMGoulart <budiarthur@gmail.com>
2021-07-09 18:50:12 -0300

    Realizando requisito 3

--- script.js
+++ script.js
@@ -36,14 +36,15 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  const carrinho = document.querySelector('.cart__items');
+  carrinho.removeChild(event.target);
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', (event) => cartItemClickListener(event));
   return li;
 }
 
