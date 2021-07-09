c7090cdd722e3a205aa50b0ac456a1069ef4a39f
Debora Teodorico <debora.steodorico@gmail.com>
2021-07-09 17:21:51 -0300

    Adicionado a questao 2 e 3

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const apiMercadoLivre = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,21 +30,36 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+const getCartComputer = async (id) => {
+  const api = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const apiJason = await api.json();
+  return apiJason;
+};
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const buttonAddCards = () => {
+  const classSection = document.querySelector('.items');
+  classSection.addEventListener('click', async (evento) => {
+    if (evento.target.className === 'item__add') {
+      const buttonPai = evento.target.parentElement;
+      const buttonId = getSkuFromProductItem(buttonPai);
+      const buttonData = await getCartComputer(buttonId);
+      const computer = createCartItemElement(buttonData);
+      document.querySelector('.cart__items').appendChild(computer);
+    }
+  });
+};
 
 async function fetchApi() {
   const response = await fetch(apiMercadoLivre);
@@ -60,4 +79,5 @@ async function fetchApi() {
 
 window.onload = () => {
   fetchApi();
+  buttonAddCards();
 };
