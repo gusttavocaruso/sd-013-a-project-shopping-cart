f2c95cebba5a97cc47109e045f78f59a1e75622a
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-07-08 21:17:06 -0300

    Resolve requisitos 2 e 3

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
+// Requisito 1
 // Recupera array Results do JSON e puxa a função createProductItem para adicioná-los ao HTML 
 const fetchComputers = async () => {
   const fetchAPI = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
@@ -36,22 +37,49 @@ const fetchComputers = async () => {
   });
 };
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+// ===========================================================================================
+// Requsito 3
+// ===========================================================================================
+function cartItemClickListener(event) { // a função já recebe o evento como parâmetro, então a partir do event.target (onde foi clicado), usa o método remove();
+  event.target.remove();
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+// ===========================================================================================
+// Requisito 2
+// ===========================================================================================
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// Função assíncrona que recupera o JSON da AP do mercado livre, a partir do ID do produto clicado
+const apiMl = async (id) => {
+  const fetchId = await fetch(`https://api.mercadolibre.com/items/${id}`); // joga a resposta do fetch na const (objeto grande)
+  const jsonId = await fetchId.json(); // joga o objeto Json na variável
+  return jsonId;
+};
+
+const buttonEvent = () => {
+  const fatherOfAll = document.querySelector('.items'); // recupera a section pai de todas as outras sections, com classe "items";
+  fatherOfAll.addEventListener('click', async (event) => { // adiciona o evento na section pai de todos. 
+    if (event.target.className === 'item__add') { // o evento só é disparado se o clique (event.target) for feito no objeto com classe 'item__add' (botões).
+      const btnFather = event.target.parentElement; // recupera o pai do botão (section) para jogar na função getSkuFromProductItem (que recupera o ID);
+      const btnId = getSkuFromProductItem(btnFather); // função que recupera o ID
+      const getJsonApi = await apiMl(btnId); // função que recupera o JSON do API do Mercado Livre. 
+      const createLiProduct = createCartItemElement(getJsonApi); // a função createCartItemElement cria uma Li a partir dos dados do Json e adiciona um evento a cada uma delas. 
+      document.querySelector('.cart__items').appendChild(createLiProduct); // adiciona o li criado anteriormente como filho do elemento <ol class="cart__items">.
+    }
+  });
+};
 
 window.onload = () => {
   fetchComputers();
+  buttonEvent();
 };
