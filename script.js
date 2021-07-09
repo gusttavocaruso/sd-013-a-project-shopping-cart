8036b668e0726e976790df942781e832c4a9b66d
Julia Baptista <baptista.julia@yahoo.com.br>
2021-07-09 12:59:37 -0300

    Quesito 4

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // const fetch = require('node-fetch');
 
+const localStorageData = [];
+
 // Requisito 1: Resolvido com ajuda de Lanai conceição, Caroline Boaventura, Luiza Antiques, Aline Hoshino, Pedro Delicoli
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -41,15 +43,26 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+const removeCartItemFromLocalSorage = (data) => {
+  const index = localStorageData.indexOf(data);
+  localStorageData.splice(index, 1);
+  localStorage.setItem('cartList', '');
+  localStorage.setItem('cartList', JSON.stringify(localStorageData));
+};
+
 // Quesito 3: Resolvido com ajuja de Luiza Antiques
 function cartItemClickListener(event) {
+  const id = event.target.getAttribute('data-id');
   event.target.remove();
+  removeCartItemFromLocalSorage(id);
+  console.log(localStorageData);
 }
 
 // Requisito 2: Resolvido com ajuda de Lanai conceição, Caroline Boaventura, Luiza Antiques, Aline Hoshino, Pedro Delicoli
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
+  li.setAttribute('data-id', sku)
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
@@ -62,6 +75,26 @@ const getCartComputer = async (id) => {
   return apiJson;
 };
 
+// const buttonEvent = () => {
+//   const parent = document.querySelector('.items');
+//   parent.addEventListener('click', async (event) => {
+//     if (event.target.className === 'item__add') {
+//       const btnParent = event.target.parentElement;
+//       const btnId = getSkuFromProductItem(btnParent);
+//       const btnData = await getCartComputer(btnId);
+//       const computer = createCartItemElement(btnData);
+//       document.querySelector('.cart__items').appendChild(computer);
+//     }
+//   });
+// };
+
+//Quesito 3
+const addCartItemToLocalSorage = (data) => {
+  localStorage.setItem('cartList', '');
+  localStorage.setItem('cartList', JSON.stringify(localStorageData));
+};
+
+// Requisito 2: Resolvido com ajuda de Lanai conceição, Caroline Boaventura, Luiza Antiques, Aline Hoshino, Pedro Delicoli
 const buttonEvent = () => {
   const parent = document.querySelector('.items');
   parent.addEventListener('click', async (event) => {
@@ -71,11 +104,26 @@ const buttonEvent = () => {
       const btnData = await getCartComputer(btnId);
       const computer = createCartItemElement(btnData);
       document.querySelector('.cart__items').appendChild(computer);
+      localStorageData.push(btnId);
+      console.log(localStorageData);
+      addCartItemToLocalSorage(localStorageData);
     }
   });
+};
+
+const getCartFromLocalSorage = () => {
+  const list = JSON.parse(localStorage.getItem('cartList'));
+  if (list) {
+    list.forEach(async (id) => {
+      const btnData = await getCartComputer(id);
+      const computer = createCartItemElement(btnData);
+      document.querySelector('.cart__items').appendChild(computer);
+    });
+  }
 };
 
 window.onload = () => {
   createProductList();
   buttonEvent();
+  getCartFromLocalSorage();
 };
