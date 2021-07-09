05b07e1df644fdc9170173befa470fe5029f80eb
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-07-09 19:29:53 -0300

    2 - Adicione o produto ao carrinho de compras

--- script.js
+++ script.js
@@ -28,19 +28,43 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return item.querySelector('span.item__sku').innerText;
 } */
 
-/* function cartItemClickListener(event) {
-  // coloque seu código aqui
-} */
-
-/* function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
 
-const addProductList = (itens) => {
+async function cartWithItem(id) {
+  return fetch(`https://api.mercadolibre.com/items/${id}`)
+  .then((resolve) => resolve.json())
+  .then((json) => {
+    const specifications = {
+      sku: json.id,
+      name: json.title,
+      salePrice: json.price,
+    };
+      const cartItem = createCartItemElement(specifications);
+      const cartItens = document.querySelector('.cart__items');
+      cartItens.appendChild(cartItem);
+  });
+}
+
+function cartItemClickListener(event) { 
+  if (event.target.className === 'item__add') {
+    const id = event.target.parentNode.firstChild.innerText;
+    cartWithItem(id);
+  }
+}
+
+const eventWithItem = () => {
+  const allItems = document.querySelector('.items');
+  allItems.addEventListener('click', (event) => {
+    cartItemClickListener(event);
+  });
+};
+
+const addItemList = (itens) => {
   itens.forEach((item) => {
     const itemElement = createProductItemElement(item);
     const section = document.querySelector('.items');
@@ -48,23 +72,18 @@ const addProductList = (itens) => {
   });
 };
 
-const productList = (query) => {
+const itemList = (query) => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
     .then((response) => {
       response.json().then((data) => {
-        addProductList(data.results);
+        addItemList(data.results);
       });
     });
 };
 
-/*   const addProduct = (ItemID) => {
-    const button = document.querySelector('')
-    fetch(`https://api.mercadolibre.com/items/${ItemID}`)
-      .then((response) => response.json()
-      .then((page) => page. ) 
-      )} 
-
-    addProduct() */
-
 window.onload = () => { };
-  productList('computador');
+  itemList('computador');
+  eventWithItem();
+
+// Requisito 1 - Havia feito dentro de uma função só, mas ficou bem melhor quando dividi o código fazendo semelhante a forma como o tio Jack fez;
+// Requisito 2 - Fiz a função eventWithItem que um evento para os meus itens, ela chama a função cartItemClickListener que vai verificar se a classe do item target (item que estou clicando) é igual a classe já pré definida 'item__add. Se sim, é criada uma constante chamada id que recebe o texto do primeiro filho do seu elemento pai, esta função envoca a função cartWithItem com o id como parametro. Esta função é async (não havia pensado em usar, mas apareceu ... ao lado do parametro dizendo que é uma função assync), ela faz a consulta a API, retorna positivamente, extraio apenas o json dela e atribuo as especificações de cada item (sku, name, salePrice) os respectivos dados json vindos da API. 
