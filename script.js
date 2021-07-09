e8b11d7a5083772935e38ddcc3799c94054606af
Julia Baptista <baptista.julia@yahoo.com.br>
2021-07-09 11:30:07 -0300

    Quesito 2 e 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // const fetch = require('node-fetch');
 
-// Requisito 1: Resolvido com ajuda de Lanai, Caroline Boaventura, Luiza Antiques, Aline Hoshino, Pedro Delicoli
+// Requisito 1: Resolvido com ajuda de Lanai conceição, Caroline Boaventura, Luiza Antiques, Aline Hoshino, Pedro Delicoli
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -36,22 +36,46 @@ const createProductList = async () => {
   document.querySelector('.items').appendChild(createProductItemElement(product)));
 };
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+// Parte de Quesito 2:
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+// Quesito 3: Resolvido com ajuja de Luiza Antiques
+function cartItemClickListener(event) {
+  event.target.remove();
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+// Requisito 2: Resolvido com ajuda de Lanai conceição, Caroline Boaventura, Luiza Antiques, Aline Hoshino, Pedro Delicoli
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const getCartComputer = async (id) => {
+  const api = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const apiJson = await api.json();
+  // console.log(apiJson);
+  return apiJson;
+};
+
+const buttonEvent = () => {
+  const parent = document.querySelector('.items');
+  parent.addEventListener('click', async (event) => {
+    if (event.target.className === 'item__add') {
+      const btnParent = event.target.parentElement;
+      const btnId = getSkuFromProductItem(btnParent);
+      const btnData = await getCartComputer(btnId);
+      const computer = createCartItemElement(btnData);
+      document.querySelector('.cart__items').appendChild(computer);
+    }
+  });
+};
 
 window.onload = () => {
   createProductList();
+  buttonEvent();
 };
