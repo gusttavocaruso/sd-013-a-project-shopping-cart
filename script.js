602eca4a179cfcbfea486799a571dba84dd2d724
Japa <wesleyy_@live.com>
2021-07-09 14:06:02 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -28,30 +28,47 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener() {
+  // coloque seu código aqui
+}
 
-// function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
-const fetchAPI = (product) => {
+const fetchAPI = async (product) => {
   const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
-  fetch(API_URL)
+   await fetch(API_URL)
     .then((response) => response.json())
     .then((data) => {
-      data.results.forEach((item) => {
+       data.results.forEach((item) => {
         const itemsSection = document.querySelector('.items');
         itemsSection.appendChild(createProductItemElement(item));
       });
     });
  };
 
-window.onload = () => {
-  fetchAPI('computador');
+const fetchItems = () => {
+  const getButtons = document.querySelectorAll('.item__add');
+  getButtons.forEach((button) => {
+    button.addEventListener(('click'), (event) => {
+      const itemId = event.target.parentNode.firstChild.innerText;
+      const API_ITEM = `https://api.mercadolibre.com/items/${itemId}`;
+      fetch(API_ITEM)
+      .then((response) => response.json()
+      .then((data) => {
+        const cartItems = document.querySelector('.cart__items');
+        cartItems.appendChild(createCartItemElement(data));
+      }));
+    });
+  });
+};
+
+window.onload = async () => {
+  await fetchAPI('computador');
+  fetchItems();
 };
