9b72017f9506b9a555101839c63545abc67d8366
mcamillosf <matheuscamillo@hotmail.com.br>
2021-07-08 23:12:32 -0300

    resolve desafio 1. Criar lista de produtos

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -24,20 +24,55 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
-window.onload = () => { };
+const addItems = (items) => {
+  items.forEach((item) => {
+    const itemHTML = createProductItemElement(item);
+    const section = document.querySelector('.items');
+    section.appendChild(itemHTML);
+  });
+};
+
+const getProductPromise = (product) => {
+  const promise = new Promise((resolve, reject) => {
+    if (product !== 'computador') {
+      reject(new Error('Produto incorreto!'));
+    } else {
+      fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+        .then((response) => {
+          response.json().then((data) => {
+            addItems(data.results);
+            resolve();
+          });
+        });
+    }
+  });
+  return promise;
+};
+
+const fetchProductPromise = async () => {
+  try {
+    await getProductPromise('computador');
+  } catch (error) {
+  console.log(error);
+  }
+};
+
+window.onload = () => {
+  fetchProductPromise();
+};
