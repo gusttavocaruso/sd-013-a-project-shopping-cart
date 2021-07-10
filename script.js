a3f6da17686e5c6f4b2f7cfa772943a2ec6145a6
PC <sthecso>
2021-07-09 22:07:30 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-//   return section;
-// }
+  return section;
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -40,4 +40,21 @@
 //   return li;
 // }
 
-// window.onload = () => { };
+// Requisito 1
+const addProductList = (products) => {
+  products.forEach((product) => {
+    const element = createProductItemElement(product);
+    const sectionList = document.querySelector('.items');
+    sectionList.appendChild(element);
+  });
+};
+
+const getJson = (query) => new Promise(() => {
+    fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+      .then((response) => response.json()) 
+      .then(((data) => addProductList(data.results)));
+  }); 
+
+window.onload = () => { 
+  getJson('computador');
+};
