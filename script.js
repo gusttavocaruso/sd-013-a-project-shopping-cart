fb6ee18f7235c45e4ccacc790c2680413701a67f
DouglasDrozda <lowdouglas9712@gmail.com>
2021-07-10 17:35:06 -0300

    Feito requisito 2

--- script.js
+++ script.js
@@ -24,21 +24,21 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener() {
+  // coloque seu código aqui
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
 const addItemsToSection = (items) => {
   items.forEach((item) => {
@@ -57,6 +57,30 @@ const fetchML = (query) => {
   });
 };
 
+const fetchItemCart = (element) => {
+  const elementPai = element.target.parentElement;
+  const getId = getSkuFromProductItem(elementPai);
+  fetch(`https://api.mercadolibre.com/items/${getId}`)
+  .then((response) => {
+    response.json()
+    .then((data) => {
+      const getLi = createCartItemElement(data);
+      const olCart = document.querySelector('.cart__items');
+      olCart.appendChild(getLi);
+    });
+  });
+};
+
+const buttonItem = () => {
+  const section = document.querySelector('.items');
+  section.addEventListener('click', (element) => {
+    if (element.target.className === 'item__add') {
+      return fetchItemCart(element);
+    }
+  });
+};
+
 window.onload = () => {
   fetchML('computador');
+  buttonItem();
  };
