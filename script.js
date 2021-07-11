b0dd4663da1bced8870d5e0ade3024b3ce915683
Adriana Barreto <adri92barreto123@gmail.com>
2021-07-10 21:19:51 -0300

    Executa primeia e segunda questao

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -24,15 +24,23 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+function createSection(itens) {
+  itens.forEach((item) => {
+    const createItem = createProductItemElement(item);
+    const section = document.querySelector('.items');
+    section.appendChild(createItem);
+  });
+}
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
+function cartItemClickListener() {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -40,4 +48,36 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+const lista = (query) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+  .then((response) => response.json()
+  .then((data) => createSection(data.results)));
+};
+
+const fetchId = (event) => {
+  const elementoPai = event.target.parentElement;
+  const idElement = getSkuFromProductItem(elementoPai);
+  fetch(`https://api.mercadolibre.com/items/${idElement}`)
+  .then((response) => response.json())
+  .then((data) =>  {
+     const addLi = createCartItemElement(data);
+     const lista = document.querySelector('.cart__items');
+     lista.appendChild(addLi);
+  })
+}
+
+function escutarClick () {
+  const itemList = document.querySelector('.items');
+  itemList.addEventListener('click', (event) => {
+    if(event.target.className === 'item__add') {
+      fetchId(event);
+    }
+  })
+}
+
+window.onload = () => {
+  lista('computador');
+  escutarClick();
+ };
+
+ // Função 1 e 2 feita com ajuda de Matheus Macêdo
