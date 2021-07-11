109a35df368898c49023d9d017d8e9c332ba5365
turtore <brunosap@gmail.com>
2021-07-11 16:14:01 -0300

    adiciondos requisitos 2 3 e 4 com erro no lint

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -24,20 +24,107 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+   const myObj = event.target;
+   console.log(myObj.sku);
+   localStorage.removeItem(myObj.sku);
+   myObj.remove();
+   priceCartUpdate();
+  }
+  
+  function createCartItemElement(sku, name, salePrice) {
+      const li = document.createElement('li');
+      li.className = 'cart__item';
+      li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+      li.sku = sku;
+      li.salePrice = salePrice;
+      document.querySelector('.cart__items').appendChild(li);
+      li.addEventListener('click', cartItemClickListener);
+      localStorage.setItem(sku, salePrice);      
+      totalPriceUpdate();
   return li;
 }
 
-window.onload = () => { };
+const addItensToSection = (items) => {
+  items.forEach((item) => {
+    const itemElement = createProductItemElement(item); // item == ao produto
+    const section = document.querySelector('.items');
+    section.appendChild(itemElement);
+  });
+};
+
+const fetchML = (query) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+  .then((response) => { // response traz todas as informações
+    response.json().then((data) => { // filtrar apenas o json
+      addItensToSection(data.results);
+      // console.log(data.results);      
+    });
+  });
+};
+
+// funcoes adicionadas
+function listToCartID(item) {
+  // console.log(item);
+    fetch(`https://api.mercadolibre.com/items/${item}`)
+    .then((response) => {
+      response.json().then((data) => {
+        // console.log(data.base_price);
+        createCartItemElement(data.id, data.title, data.base_price);
+        // console.log(data); //aqui ta a resposta completa do produto
+      });
+    });  
+}
+
+// preco do carrinho
+const priceCartUpdate = () => {
+  let cartPrice = 0;
+  const cartI = document.querySelectorAll('.cart__item');
+  for (let i = 0; i < cartI.length; i += 1) {
+    cartPrice += cartI[i].salePrice;
+    // console.log(cartI[i].salePrice);
+  }  
+  return cartPrice;
+};
+
+const totalPriceUpdate = () => {
+  const clearUp = document.querySelector('.total-price');
+  if (clearUp !== null) {
+    clearUp.remove();
+  }
+  const h2 = document.createElement('h2');
+  h2.classList = 'total-price';
+  h2.innerText = `Total: R$ ${priceCartUpdate()}`;
+  document.querySelector('.cart').appendChild(h2);
+};
+
+const testStorageKey = (key) => {
+  const test = 'MLB';
+  if (key.indexOf(test) > -1) {
+    listToCartID(key);
+    // console.log(localStorage[key]);
+  }
+};
+
+const clearCart = () => Storage.clear();
+
+const emptyCart = document.querySelector('.empty-cart');
+// emptyCart.addEventListener("click", clearCart);
+
+window.onload = () => { 
+  fetchML('computador');
+  
+  document.querySelector('.items').addEventListener('click', function (e) {
+    if (e.target.classList.contains('item__add')) {
+      listToCartID(e.target.parentNode
+        .querySelector('.item__sku').innerText);
+      }
+    });
+    for (const key in localStorage) {
+      testStorageKey(key);
+    }
+  };
