f2356a91ccadf95f48f8f418bacf26f401917d3b
Demistocles <demi.treze@gmail.com>
2021-07-12 18:10:03 -0300

    requisito 1

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -24,20 +24,46 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
-window.onload = () => { };
+const addProduct = (items) => {
+  items.forEach((item) => {
+    const itemHTML = createProductItemElement(item);
+    const section = document.querySelector('.items');
+    section.appendChild(itemHTML);
+  });
+};
+
+const getProduct = (product) => {
+  const promise = new Promise((resolve, reject) => {
+    if (product !== 'computador') {
+      reject(new Error('Produto incorreto'));
+    } else {
+      fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+        .then((response) => {
+          response.json().then((data) => {
+            addProduct(data.results);
+          });
+        });
+    }
+  });
+  return promise;
+};
+
+window.onload = () => {
+  getProduct('computador');
+ };
