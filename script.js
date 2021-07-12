b07aabcb2886a166b6cfff41657801fd23394594
Filipe <filipecandido123@hotmail.com>
2021-07-12 17:48:22 -0300

    allRequisito

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+// Button GLOBAL
 const carShop = document.querySelector('.cart__items');
-// const priceOfPay = document.querySelector('.total-price');
-
+const priceOfPay = document.querySelector('.total-price');
+// Requisito 4 setItem
 const saveLs = () => {
   localStorage.setItem('Produto', carShop.innerHTML);
-  // localStorage.setItem('Ṕrice', priceOfPay.innerHTML);
+  localStorage.setItem('Price', priceOfPay.innerHTML);
 };
-
+// Recupero o localStorage
 const catchCarShop = () => {
   carShop.innerHTML = localStorage.getItem('Produto');
-  // priceOfPay.innerHTML = localStorage.getItem('Price');
+  priceOfPay.innerHTML = localStorage.getItem('Price');
+};
+
+// requisito 5
+const totalPrice = async () => {
+  let price = 0;
+  const allCarShop = document.querySelectorAll('.cart__item');
+  allCarShop.forEach((item) => {
+    const priceTheItems = item.innerText.split('$');
+    price += Number(priceTheItems[1]);
+  });
+  priceOfPay.innerHTML = `${Math.round((price * 100) / 100)}`;
 };
 
 function createProductImageElement(imageSource) {
@@ -29,23 +41,22 @@ function createCustomElement(element, className, innerText) {
 function cartItemClickListener(event) {
   const evento = event.target;
   evento.remove();
+  totalPrice();
   saveLs();
 }
+
+carShop.addEventListener('click', cartItemClickListener); // requisito 3, evento pra remover um item criado
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener); // requisito 3, evento pra remover um item criado
- 
+
   const getOl = document.querySelector('.cart__items');
   getOl.appendChild(li);
+  totalPrice();
   saveLs();
 }
-
-// const totalPrice = () => {
-
-// }
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -66,6 +77,7 @@ const buttonEmptyCar = document.querySelector('.empty-cart');
 
 const emptyCar = () => {
   carShop.innerHTML = '';
+  totalPrice();
   saveLs();
 };
 
@@ -111,5 +123,7 @@ const getProdutos = async () => {
   }
 };
 
-catchCarShop();
-getProdutos();
+window.onload = () => {
+  catchCarShop();
+  getProdutos();
+};
