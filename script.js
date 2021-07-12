33111104d44c54acfe32b97406f090d72bac8aa6
Jéssica Queiroz <gnr0034@gmail.com>
2021-07-12 17:48:35 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -42,15 +42,16 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// function cartItemClickListener(event) {
+function cartItemClickListener(event) {
   // coloque seu código aqui
-// }
+  event.target.remove();
+}
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
