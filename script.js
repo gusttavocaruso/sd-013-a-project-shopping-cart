8ea3c43b085674d32646eb1b42668061875bab8c
Bruno <brunoalencarlima@outlook.com.br>
2021-07-12 17:01:14 -0300

    exer 1, 2

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
+  section.id = sku;
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
@@ -29,10 +30,10 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -40,4 +41,37 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+const addItensToSection = (items) => {
+  items.forEach((item) => {
+    const itemElement = createProductItemElement(item);
+    const section = document.querySelector('.items');
+    section.appendChild(itemElement);
+  });
+};
+
+const fetchML = (query) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+    .then((response) => {
+      response.json().then((data) => {
+        addItensToSection(data.results);
+      });
+    });
+};
+
+const addClick = (id) => {
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+      const novoProduto = createCartItemElement(data);
+        document.querySelector('.cart__items').appendChild(novoProduto);
+      });
+    });
+};
+
+window.onload = () => {
+  fetchML('computador');
+  document.querySelector('.items').addEventListener('click', (event) => {
+    if (event.target.className === 'item__add') addClick(event.target.parentNode.id);
+  });   
+};
