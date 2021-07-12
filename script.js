f7fa909d2c727bd15265d750449e42f5202e4279
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-07-12 20:00:45 -0300

    Resolve os 80%

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const ol = document.querySelector('.cart__items');
+const arr = [];
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -28,18 +29,38 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   //   return item.querySelector('span.item__sku').innerText;
   // }
   
+const sub = (string) => {
+  const ultimoElement = string.innerText.split(' ');
+  const elementoComCifrao = ultimoElement[ultimoElement.length - 1].replace('$', '');
+  const semCifrao = elementoComCifrao;
+  const span = document.querySelector('.total-price');
+  const newValue = parseFloat(span.innerHTML) - parseFloat(semCifrao);
+  span.innerHTML = Math.round(newValue * 100) / 100;
+};
+
 function cartItemClickListener(event) {
   // coloque seu código aqui
   const itemToRemove = event.target;
   itemToRemove.remove();
   localStorage.cartList = ol.innerHTML;
+  sub(itemToRemove);
 }
+
+const sumOfPrices = (key) => {
+  arr.push(key.salePrice);
+  console.log(arr);
+  const totalPrice = arr.reduce((sum, acc) => (sum + acc), 0);
+  const result = (totalPrice * 100) / 100;
+  const span = document.querySelector('.total-price');
+  span.innerHTML = result;
+};
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  sumOfPrices({ salePrice });
   return li;
 }
 
@@ -48,7 +69,6 @@ const achaID = async (event) => {
   const response = await fetch(`https://api.mercadolibre.com/items/${idProduct}`);
     const data = await response.json();
     const itensCompraveis = createCartItemElement(data);
-    console.log(itensCompraveis);
     ol.appendChild(itensCompraveis);
     localStorage.cartList = ol.innerHTML;
     // sumOfPrices(itensCompraveis);
@@ -60,8 +80,8 @@ const fetchAPI = () => new Promise(() => {
   .then((computer) => {
     // Recebi Ajuda do Luiz Furtado
     const itemList = document.querySelector('.items');
-    const arr = computer.results;
-    arr.forEach((obj) => {
+    const arra = computer.results;
+    arra.forEach((obj) => {
       const objs = createProductItemElement(obj);
       itemList.appendChild(objs);
       objs.lastChild.addEventListener('click', achaID);
