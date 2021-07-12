e3f7aa98df01844a5b7911bcaa284be5d2936730
Seu nomecd <canaldogildosantos@gmail.com>
2021-07-12 02:03:55 -0300

    resolve o desafio 1

--- script.js
+++ script.js
@@ -1,43 +1,79 @@
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+function createProductImageElement(imageSource) {     // função para exercico 1: cria imagens
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+function createCustomElement(element, className, innerText) {    // função: cria elemnetos html
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-//   return section;
-// }
+function createProductItemElement({ id:sku, title:name, thumbnail:image }) { //função cria a section, ex.1
+  const section = document.createElement('section');
+  section.className = 'item';
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));    // apendi ex.1
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
 // function cartItemClickListener(event) {
 //   // coloque seu código aqui
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
-// window.onload = () => { };
+//resolve desafio1 - (com ajuda da Aline Hoshino)
+const addProducts = (itens) => {
+  itens.forEach((item) => {
+    const product = createProductItemElement(item);
+    const section = document.querySelector('.items');
+    section.appendChild(product);
+  });
+};
+
+const mercadoLivre = ((query) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+  .then((resposta) => {
+  resposta.json().then((data)=> {
+   addProducts(data.results);
+  }); 
+  });
+});
+
+//desafio 2
+const mercado = (query) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+    .then((response) => {
+      response.json()
+    });
+}
+document.querySelector(".item__add")
+
+
+
+
+
+
+ window.onload = () => { 
+  mercadoLivre ('computador');
+};
