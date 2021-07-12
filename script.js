65b06e22ee4a8fac0568468898029e612269cfb3
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-07-12 20:00:13 -0300

    linter

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (element === 'button') {
+    e.addEventListener('click', addCart);
+  }
   return e;
 }
 
@@ -24,21 +27,43 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText; // captura id do elemento
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+ event.target.remove(); // Requisito 3
+}
 
-// function createCartItemElement({ id: sku, title: name, thumbnail: salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const addCart = (event) => {
+  const skuId = event.target.parentNode.firstChild.innerText;
+  fetch(`https://api.mercadolibre.com/items/${skuId}`)
+    .then((response) => {
+        response.json().then((data) => {
+        const { sku, name, salePrice } = data;
+        console.log(data);
+        const cartItem = createCartItemElement(data);
+        const cartItems = document.querySelector('.cart__items');
+        cartItems.appendChild(cartItem);
+      });
+    });
+};
+
+// Susbtituido na função que cria o botão
+// const clickAdd = () => {
+//   const buttons = document.querySelectorAll('.item__add');
+//   buttons.forEach((button) => {
+//     button.addEventListener('click', addCart);
+//   });
+// };
 
 const addItemsToSection = (items) => {
   items.forEach((item) => {
@@ -59,4 +84,5 @@ const fetchML = (query) => {
 
 window.onload = () => {
 fetchML('computador');
+// clickAdd();
 };
