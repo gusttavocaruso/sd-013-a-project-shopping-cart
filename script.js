b8660492d6db765c5068329c2f12fddaaa609068
Seu nomecd <canaldogildosantos@gmail.com>
2021-07-12 21:37:11 -0300

    resolve desafios: 2, 3, e 4

--- script.js
+++ script.js
@@ -1,40 +1,65 @@
+const string = '.cart__items';
 
-
-function createProductImageElement(imageSource) {     // função para exercico 1: cria imagens
+// função para exercico 1: cria imagens
+function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
 
-function createCustomElement(element, className, innerText) {    // função: cria elemnetos html
+// função: cria elemnetos html
+function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
 
-function createProductItemElement({ id:sku, title:name, thumbnail:image }) { //função cria a section, ex.1
+//ex.1: função cria a section, e adiciona os filhos.
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));    // apendi ex.1
+  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
 }
 
+//cria uma linha com elemento, através do ID do elemento.
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+//desafio4
+const local = () => {
+  const ol = document.querySelector(string);
+  const olol = ol.innerHTML;
+  localStorage.setItem('lista', olol);
+}
 
-function createCartItemElement({ sku, name, salePrice }) {
+const salva = (() => {
+  const ol = document.querySelector(string);
+  ol.innerHTML = localStorage.getItem('lista');
+  console.log(ol.children);
+  ol.addEventListener('click', (event) => { 
+  if(event.target.className === 'cart__item'){
+    cartItemClickListener(event)
+  }
+  })
+});
+
+
+//desafio3
+function cartItemClickListener(event) {
+  event.target.remove();
+  local();
+};
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -42,38 +67,53 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-//resolve desafio1 - (com ajuda da Aline Hoshino)
+//resolve desafio 1 e parte do 2 - (com ajuda da Aline Hoshino)
 const addProducts = (itens) => {
   itens.forEach((item) => {
     const product = createProductItemElement(item);
     const section = document.querySelector('.items');
     section.appendChild(product);
   });
+  const butons = document.querySelectorAll('.item__add')
+  butons.forEach((button) => {
+    button.addEventListener('click', getId)
+  })
+
 };
 
 const mercadoLivre = ((query) => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
-  .then((resposta) => {
-  resposta.json().then((data)=> {
-   addProducts(data.results);
-  }); 
-  });
+    .then((resposta) => {
+      resposta.json().then((data) => {
+        addProducts(data.results);
+      });
+    });
 });
 
-//desafio 2
-const mercado = (query) => {
-  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+//desafio 2 ajuda de GAbriel
+const funcFetch = (query) => {
+  fetch(`https://api.mercadolibre.com/items/${query}`)
     .then((response) => {
-      response.json()
+      return response.json()
+    })
+    .then((data) => {
+      const li = createCartItemElement(data)
+      const ol = document.querySelector(string);
+      ol.appendChild(li)
+      local();
+
     });
+
+};
+
+const getId = (event) => {
+  const idElement = getSkuFromProductItem(event.target.parentElement)
+  funcFetch(idElement)
+
 }
-document.querySelector(".item__add")
 
 
-
-
-
-
- window.onload = () => { 
-  mercadoLivre ('computador');
+window.onload = () => {
+  mercadoLivre('computador');
+  salva();
 };
