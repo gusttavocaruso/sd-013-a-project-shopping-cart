5428682f967df052076f171f75d64975f39282b1
Gabriel Lenz <lenzordmusic@gmail.com>
2021-07-12 20:53:06 -0400

    Requisito 4

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ const saveLocal = () => {
   localStorage.setItem('lista', htmlText);
 };
 
+function cartItemClickListener(event) {
+  event.target.remove();
+  saveLocal();
+}
+
+const getItem = () => {
+  const ol = document.querySelector(string);
+  ol.innerHTML = localStorage.getItem('lista');
+  const olChildren = [...ol.children];
+  olChildren.forEach((li) => {
+    li.addEventListener('click', cartItemClickListener);
+  });
+};
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -28,17 +42,12 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  
+
   return section;
 }
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  event.target.remove();
-  saveLocal();
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
@@ -57,28 +66,28 @@ const appendData = (resultado) => {
   });
 };
 
- const fetchML = (item) => {
+const fetchML = (item) => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`)
-  .then((response) => {
-    response.json().then((data) => {
-      appendData(data.results);
+    .then((response) => {
+      response.json().then((data) => {
+        appendData(data.results);
+      });
     });
-  });
- };
+};
 
 const fetchItemCart = (element) => {
   const elementoPai = element.target.parentElement;
   const getId = getSkuFromProductItem(elementoPai);
   fetch(`https://api.mercadolibre.com/items/${getId}`)
-  .then((response) => {
-    response.json()
-    .then((data) => {
-      const getLi = createCartItemElement(data);
-      const olCart = document.querySelector(string);
-      olCart.appendChild(getLi);
-      saveLocal();
+    .then((response) => {
+      response.json()
+        .then((data) => {
+          const getLi = createCartItemElement(data);
+          const olCart = document.querySelector(string);
+          olCart.appendChild(getLi);
+          saveLocal();
+        });
     });
-  });
 };
 
 const button = () => {
@@ -88,11 +97,6 @@ const button = () => {
       fetchItemCart(element);
     }
   });
-};
-
-const getItem = () => {
-  const ol = document.querySelector(string);
-  ol.innerHTML = localStorage.getItem('lista');
 };
 
 window.onload = () => {
