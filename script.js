54a3d1ec8d310ae1278a6d78b8b4489e1c75650f
Emídio Rebouças <emidioreb@gmail.com>
2021-07-13 14:51:58 -0300

    requisito 2 ok

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
     return section;
   }
 
@@ -31,14 +30,16 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 //   // coloque seu código aqui
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
-
+function createCartItemElement({ id, title, price }) { // desestruturação feita. É selecionado apenas o id, title e price.
+  const li = document.createElement('li'); // criação da const 'li'
+  li.className = 'cart__item'; // add classe na 'li'
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`; // Isso é para pegar apenas o texto de cada elemento
+  // li.addEventListener('click', cartItemClickListener);
+  const cartItens = document.querySelector('.cart__items'); // Criação da const referente a classe cart__items
+  cartItens.appendChild(li); // li vai ser filho do cartItens
+  return li;
+}
+// Requisito 1
 const addItems = (items) => { // criar itens la no html
   items.forEach((item) => {
     const itemElement = createProductItemElement(item);
@@ -46,13 +47,31 @@ const addItems = (items) => { // criar itens la no html
     section.appendChild(itemElement);
   });
 };
-
+// Requisito 2  requisição para o endpoint
+const addProductShoppingCart = (event) => { // Essa função é ativada quando o botão é clicado. 
+  const id = event.target.parentNode.firstChild.innerText; // Essa const é criada para pegar o ID de quando é clicado no produto(vou até o pai e pego o primeiro filho)
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((response) => { // Permissão
+      response.json()
+      .then((data) => { // Permissão
+        createCartItemElement(data); // Eu vou pegar esses e mandar para essa função da linha 34.
+      });
+    });
+};
+// Requisito 2 => criação do botão. Após o clique o botão vai realizar uma requisição para a função addProductShoppingCart
+const buttonItem = () => {
+  const buttons = document.querySelectorAll('.item__add'); // Aqui está sendo capturado todos os botões
+// Logo abaixo, foi add um forEach para que o addEventListener fosse passa por todos os botões.
+  buttons.forEach((button) => button.addEventListener('click', addProductShoppingCart));
+};
+// Requisito 1
 const productList = (product) => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)// vai até o site atrás das informações através de uma permissão
     .then((response) => { // permissão
       response.json()
       .then((data) => { // permissão
       addItems(data.results);// Resultado dos computadores
+      buttonItem(); 
       });
     });
 };
