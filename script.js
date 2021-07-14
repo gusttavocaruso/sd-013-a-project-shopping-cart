51c7e4653e6599abef4d20f98a63490fbf94db56
Bruno <brunoalencarlima@outlook.com.br>
2021-07-13 23:06:24 -0300

    exer 6

--- script.js
+++ script.js
@@ -29,8 +29,18 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+const deletar = (idItem) => {
+  const produtos = document.querySelectorAll('li');
+  const newObjeto = { item: [] };
+  produtos.forEach((produto) => {
+    if (produto.id !== idItem) newObjeto.item.push(produto.textContent);
+  });
+  localStorage.setItem('item', JSON.stringify(newObjeto));
+};
+
 function cartItemClickListener(event) {
   event.target.remove();
+  deletar(event.target.id);
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
@@ -38,6 +48,7 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  li.id = document.querySelectorAll('.cart__item').length;
   return li;
 }
 
@@ -58,19 +69,48 @@ const fetchML = (query) => {
     });
 };
 
+const save = () => {
+  const lista = { item: [] };
+  document.querySelectorAll('.cart__item').forEach((li) => {
+    lista.item.push(li.textContent);
+  });
+  localStorage.setItem('item', JSON.stringify(lista));
+};
+
 const addClick = (id) => {
   fetch(`https://api.mercadolibre.com/items/${id}`)
     .then((response) => {
       response.json().then((data) => {
         const novoProduto = createCartItemElement(data);
         document.querySelector('.cart__items').appendChild(novoProduto);
+        save();
       });
     });
 };
 
+const load = () => {
+  const object = JSON.parse(localStorage.getItem('item'));
+  Object.values(object).forEach((liContent) => {
+    const creatLi = document.createElement('li');
+    creatLi.textContent = liContent;
+    creatLi.addEventListener('click', cartItemClickListener);
+    document.querySelector('.cart__items').appendChild(creatLi);
+  });
+};
+const clearList = () => {
+  document.querySelectorAll('li').forEach((li) => li.remove());
+  localStorage.clear();
+};
+
 window.onload = () => {
+  if (Storage) {
+    const object = JSON.parse(localStorage.getItem('item'));
+    if (object) load();
+  }
   fetchML('computador');
   document.querySelector('.items').addEventListener('click', (event) => {
     if (event.target.className === 'item__add') addClick(event.target.parentNode.id);
   });
+
+  document.querySelector('.empty-cart').addEventListener('click', clearList);
 };
