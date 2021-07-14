8f9e4982f75d08b268d4d35958d7fc8536d660ab
Caê <eduardokae01@hotmail.com>
2021-07-14 11:11:42 -0300

    refiz requisito 1 - Requisito 2

--- script.js
+++ script.js
@@ -24,35 +24,63 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+// // function getSkuFromProductItem(item) {
+// //   return item.querySelector('span.item__sku').innerText;
+// // }
 
 // function cartItemClickListener(event) {
-//   // coloque seu código aqui
+//   console.log(event.target);
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
-const addItens = (itens) => {
+const createItens = (itens) => {
   itens.forEach((item) => {
-    const createItem = createProductItemElement(item);
+    const create = createProductItemElement(item);
     const section = document.querySelector('.items');
-    section.appendChild(createItem);
+    section.appendChild(create);
   });
 };
 
-const getItensML = (product) => { //  Requisito 1 resolvido com ajuda de Jackson Pires - Professor turma 13 A
+const createItemToCart = (data) => {
+  const test = createCartItemElement(data);
+  console.log(test);
+  const ol = document.querySelector('.cart__items');
+  ol.appendChild(test);
+};
+
+const sendToCart = (id) => {
+  const numberId = id.innerText;
+  fetch(`https://api.mercadolibre.com/items/${numberId}`)
+    .then((resolve) => {
+      resolve.json().then((data) => {
+        createItemToCart(data);
+      });
+  });
+};
+
+const getId = (e) => {
+  const itemClicado = e.path[1];
+  const idItemClicado = itemClicado.querySelector('.item__sku');
+  sendToCart(idItemClicado);
+};
+
+const getItensML = (product) => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
     .then((response) => {
       response.json().then((data) => {
-        addItens(data.results);
+        createItens(data.results);
+
+        const buttons = document.querySelectorAll('.item__add');
+        buttons.forEach((button) => {
+          button.addEventListener('click', getId);
+        });
       });
     });
 };
