f5cf8d0f700fc8ab5deacf1d71d693b30e32def0
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-07-14 19:27:53 -0300

    Resolvendo requisito 4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
+  const img = document.createElement("img");
+  img.className = "item__image";
   img.src = imageSource;
   return img;
 }
@@ -13,20 +13,22 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+  const section = document.createElement("section");
+  section.className = "item";
   section.id = sku;
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement("span", "item__sku", sku));
+  section.appendChild(createCustomElement("span", "item__title", name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement("button", "item__add", "Adicionar ao carrinho!")
+  );
 
   return section;
 }
 
 function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+  return item.querySelector("span.item__sku").innerText;
 }
 
 function cartItemClickListener(event) {
@@ -34,42 +36,70 @@ function cartItemClickListener(event) {
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
+  const li = document.createElement("li");
+  li.className = "cart__item";
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener("click", cartItemClickListener);
+  li.id = document.querySelectorAll('.cart__item').length;
   return li;
 }
 
 const results = (items) => {
   items.forEach((item) => {
     const element = createProductItemElement(item);
-    const section = document.querySelector('.items');
+    const section = document.querySelector(".items");
     section.appendChild(element);
   });
 };
 
 const search = (query) => {
-  const tests = fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+  const tests = fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?q=${query}`
+  )
     .then((response) => response.json())
     .then((body) => {
       results(body.results);
     });
 };
 
+const saveContent = () => {
+  const list = { item: [] };
+  document.querySelectorAll("li").forEach((listItem) => {
+    list.item.push(listItem.textContent);
+  });
+  localStorage.setItem('item', JSON.stringify(list));
+};
+
 const adicionaClick = (id) => {
-  fetch(`https://api.mercadolibre.com/items/${id}`)
-    .then((response) => {
-      response.json().then((data) => {
-        const newProduct = createCartItemElement(data);
-        document.querySelector('.cart__items').appendChild(newProduct);
-      });
+  fetch(`https://api.mercadolibre.com/items/${id}`).then((response) => {
+    response.json().then((data) => {
+      const newProduct = createCartItemElement(data);
+      document.querySelector(".cart__items").appendChild(newProduct);
+      saveContent();
     });
+  });
+};
+
+const load = () => {
+  const objects = JSON.parse(localStorage.getItem('item'));
+ objects.item.forEach((liContent) => {
+    const creatLi = document.createElement('li');
+    creatLi.textContent = liContent;
+    console.log(creatLi);
+    creatLi.addEventListener("click", cartItemClickListener);
+    document.querySelector('.cart__items').appendChild(creatLi);
+  });
 };
 
 window.onload = () => {
-  search('computador');
-  document.querySelector('.items').addEventListener('click', (event) => {
-    if (event.target.className === 'item__add') adicionaClick(event.target.parentNode.id);
+  if (Storage) {
+    const objects = JSON.parse(localStorage.getItem('item'));
+    if (objects) load();
+  };
+  search("computador");
+  document.querySelector(".items").addEventListener("click", (event) => {
+    if (event.target.className === "item__add")
+      adicionaClick(event.target.parentNode.id);
   });
 };
+
