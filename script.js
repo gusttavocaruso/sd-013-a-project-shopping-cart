8cb39df224ab09de3ccd73e2b3de112fd20fd6ba
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-07-14 22:29:11 -0300

    Inicia Requisito 2

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 //   // coloque seu código aqui
 // }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -48,6 +48,7 @@ const addItensToSection = (items) => {
     });
 };
 
+//Requisito 1
 const fetchMl = (query) => {
     fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
       .then((response) => {
@@ -57,6 +58,36 @@ const fetchMl = (query) => {
       });
 };
 
+const fetchId = (event) => {
+  const element = event.target.parentElement;
+  const idElement = getSkuFromProductItem(element);
+  fetch(`https://api.mercadolibre.com/items/${idElement}`)
+  .then((response) => response.json())
+  .then((data) => {
+    const adicionaLi = createCartItemElement(data);
+    const lista = document.querySelector('.cart__item');
+    lista.appendChild(adicionaLi);
+  })
+}
+
+function click () {
+  const itemList = document.querySelector('.items');
+  itemList.addEventListener('click', (event) => {
+    if(event.target.className === 'item__add') {
+      fetchId(event);
+    }
+  })
+}
+
+function createChild(itens) {
+  itens.forEach((item) => {
+    const createItem = createProductItemElement(item);
+    const child = document.querySelector('.items');
+    child.appendChild(createItem);
+  });
+}
+
 window.onload = () => { 
     fetchMl('computador');
+    click();
 };
