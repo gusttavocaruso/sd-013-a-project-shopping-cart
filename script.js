6a19f67ff17c29195966f1890b7f7dc4cfbec090
Julio Fallace <julioscoffee@gmail.com>
2021-07-18 21:23:42 -0300

    Incrementando projeto, rever addcarrinho e localstorage

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const getItems = document.querySelector('.items');
+const getCart = document.querySelector('.cart__items');
+const getEmptyButton = document.querySelector('.empty-cart');
+const loading = document.querySelector('.loading');
+const getTotalPrice = document.querySelector('.total-price');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,7 +18,52 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+const saveCart = () => {
+  localStorage.setItem('saveCart', getCart.innerHTML);
+  localStorage.setItem('savePrice', getTotalPrice.innerHTML);
+};
+
+const soma = (value, operator) => {
+  const result = value + operator;
+  getTotalPrice.innerHTML = Math.round(result * 100) / 100;
+  saveCart();
+};
+
+const sub = (value, operator) => {
+  const result = value - operator;
+  getTotalPrice.innerHTML = Math.round(result * 100) / 100;
+  saveCart();
+};
+
+const setTotalPrice = (value, operator) => {
+  const getAtualPrice = Number(getTotalPrice.innerHTML);
+  if (operator === '+') {
+    soma(getAtualPrice, value);
+  }
+  if (operator === '-') {
+    sub(getAtualPrice, value);
+  }
+};
+
+const setCartItem = ({ id: sku, title: name, price: salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart_item';
+  li.innerHTML = `SKU: ${sku} | NAME: ${name} | PRICE: $ <span>${salePrice}</span>`;
+  getCart.appendChild(li);
+  setTotalPrice(salePrice, '+');
+};
+
+const setProductCart = async (id) => {
+  try {
+    const productInfo = await (await fetch(`https://api.mercadolibre.com/items/${id}`)).json();
+    setCartItem(productInfo);
+    saveCart();
+  } catch (error) {
+    alert(error);
+  }
+};
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -20,24 +71,48 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
+  section.lastElementChild.addEventListener('click', (event) => {
+    const productID = event.target.parentElement.firstElementChild.innerText;
+    setProductCart(productID);
+  });
+  getItems.appendChild(section);
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+const removeCartItem = (event) => {
+  if (event.target.className === 'cart_item') {
+    event.target.remove();
+    const getProductPrice = event.target.querySelector('span').innerText;
+    setTotalPrice(getProductPrice, '-');
+    saveCart();
+  }
+};
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+const returnCart = () => {
+  getCart.innerHTML = localStorage.getItem('saveCart');
+  getTotalPrice.innerHTML = localStorage.getItem('savePrice');
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+getEmptyButton.addEventListener('click', () => {
+  const getFullCart = document.querySelectorAll('.cart_item');
+  getFullCart.forEach((item) => item.parentNode.removeChild(item));
+  getTotalPrice.innerHTML = 0;
+  saveCart();
+});
 
-window.onload = () => { };
+const getProducts = async (product = 'computador') => {
+  try {
+    ((await (await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`))
+      .json())
+        .results)
+    .forEach((computador) => createProductItemElement(computador), loading.remove());
+  } catch (error) {
+    alert(error);
+  }
+};
+
+getCart.addEventListener('click', (removeCartItem));
+
+window.onload = () => {
+  getProducts();
+  returnCart();
+};
