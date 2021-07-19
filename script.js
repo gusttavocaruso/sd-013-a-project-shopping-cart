a0cbb2a96f7a41e4503b5af9d55ee89001e99e33
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2021-07-19 19:33:34 -0300

    Requisitos 5 e 7

--- script.js
+++ script.js
@@ -30,6 +30,14 @@ function getSkuFromProductItem(item) {
 
 const getCart = () => document.querySelector('.cart__items');
 
+const displayTotalPrice = () => { // Questão resolvida com ajuda do amigo Rodrigo Pova.
+  const priceDisplay = document.querySelector('.total-price');
+  const cartItemsArr = [...getCart().children];
+  const totalPrice = cartItemsArr.reduce((acc, curr) => 
+    acc + Number(curr.innerText.split('$')[1]), 0);
+  priceDisplay.innerText = totalPrice;
+};
+
 function storeCart() { // Questão resolvida com ajuda do amigo Rodrigo Pova.
   localStorage.setItem('cartContent', getCart().innerHTML);
 }
@@ -42,11 +50,14 @@ function emptyButton() {
   const emptyBtn = document.querySelector('.empty-cart');
   emptyBtn.addEventListener('click', () => {
     getCart().innerHTML = '';
+    displayTotalPrice();
+    storeCart();
   });
 }
 
 function cartItemClickListener(event) { // Questão resolvida com ajuda do amigo Rodrigo Pova.
   event.target.remove();
+  displayTotalPrice();
   storeCart();
 }
 
@@ -58,12 +69,26 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   return li;
 }
 
+function loading() {
+  const loadingText = document.createElement('p');
+  loadingText.innerText = 'Loading...';
+  loadingText.className = 'loading';
+  document.querySelector('.container').appendChild(loadingText);
+}
+
+function removeLoading() {
+  document.querySelector('.loading').remove();
+}
+
 async function addNewItemToCart(event) { // Questão resolvida com ajuda do amigo Rodrigo Pova.
+  loading();
   const itemSku = getSkuFromProductItem(event.target.parentElement);
   const response = await fetch(`https://api.mercadolibre.com/items/${itemSku}`);
   const data = await response.json();
   const findOl = document.querySelector('.cart__items');
+  removeLoading();
   findOl.appendChild(createCartItemElement(data));
+  displayTotalPrice();
   storeCart();
 }
 
@@ -82,10 +107,12 @@ function addNewItem(items) { // Questão resolvida com ajuda do amigo Rodrigo Po
 
 async function getApi(userInput) {
   // Primeiramente, declaramos a função utilizando o método async/await e, por meio da const response, recebemos uma promise do site MercadoLivre. No endereço da API, substituímos a parte final da URL, por meio de template literals, para que possamos atualizar a página com base no input fornecido pelo usuário.
+  loading();
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${userInput}`);
   // Na sequência, declaramos uma constante chamada 'data' e atribuímos a ela a resposta obtida anteriormente, mas filtramos os dados utilizando o método json(). O resultado é uma nova promise que iremos retornar chamando pela constante data.
   const data = await response.json();
     // A constante data já está armazenando os dados do json() e, por tanto, basta chamar a função addNewItem e passar como parâmetro o conteúdo de data, filtrando pelos resultados.
+    removeLoading();
     addNewItem(data.results);
 }
 
