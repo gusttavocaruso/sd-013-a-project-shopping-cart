b918b23a1b0f49ed0cbce49fc909018e7699bc2d
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-07-19 12:25:17 -0300

    Requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  // const cart = '.cart__items';
+  const cart = '.cart__items';
   
   function createProductImageElement(imageSource) {
     const img = document.createElement('img');
@@ -42,24 +42,25 @@
     return item.querySelector('span.item__sku').innerText;
    }
 
-  //  function cartItemClickListener(event) {
-  //      // coloque seu código aqui
-  //  }
+   function cartItemClickListener(event) {
+       event.target.remove();
+       localStorage.setItem('cartItems', document.querySelector(cart).innerHTML);
+   }
 
    function createCartItemElement({ id: sku, title: name, price: salePrice }) {
     const li = document.createElement('li');
     li.className = 'cart__item';
     li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-    // li.addEventListener('click', cartItemClickListener);
+    li.addEventListener('click', cartItemClickListener);
     return li;
    }
 
    const addProductToCart = (obj) => {
-    const myCart = document.querySelector('.cart__items');
+    const myCart = document.querySelector(cart);
     const newLi = createCartItemElement(obj);
     myCart.appendChild(newLi);
   };
-  
+
   // Req 2
   const addEventButtons = () => {
     const section = document.querySelector('.items');
@@ -70,11 +71,24 @@
         const obj = await fetch(`https://api.mercadolibre.com/items/${itemId}`)
           .then((response) => response.json());
         addProductToCart(obj);
+        localStorage.setItem('cartItems', document.querySelector(cart).innerHTML);
       }
     });
   };
+  
+  // Req 4
+  function getLocalStorage() {
+    const myCart = document.querySelector(cart);
+    myCart.innerHTML = localStorage.getItem('cartItems');
+    myCart.addEventListener('click', (event) => {
+      if (event.target.className === 'cart__item') {
+        cartItemClickListener(event);
+      }
+    });
+  }
 
   window.onload = () => {
     createProductList();
     addEventButtons();
+    getLocalStorage();
   };
