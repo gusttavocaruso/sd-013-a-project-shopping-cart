11d291c41ea9eacafa91fee3ccc975cd871c3f8b
Gabriel Priss <gabrielpriss@hotmail.com>
2021-07-21 11:07:33 -0300

    lint resolvido e requisitos 2 ao 6

--- script.js
+++ script.js
@@ -24,30 +24,68 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-const getJson = async (query) => {
-  const fetchApi = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=$${query}`);
-  const apiJson = await fetchApi.json();
-  const products = apiJson.results; 
-  products.forEach((product) => document.querySelector('.items')
-    .appendChild(createProductItemElement(product)));
-};
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+const getCart = () => document.querySelector('.cart__items');
 
-// function cartItemClickListener(event) {
-  
-// }
+function storeCart() { 
+  localStorage.setItem('cartContent', getCart().innerHTML);
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function retrieveCart() {
+  getCart().innerHTML = localStorage.getItem('cartContent');
+}
+
+function emptyButton() {
+  const emptyBtn = document.querySelector('.empty-cart');
+  emptyBtn.addEventListener('click', () => {
+    getCart().innerHTML = '';
+  });
+}
+
+function cartItemClickListener(event) { 
+  event.target.remove();
+  storeCart();
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) { 
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function addNewItemToCart(event) {
+  const itemSku = getSkuFromProductItem(event.target.parentElement);
+  const response = await fetch(`https://api.mercadolibre.com/items/${itemSku}`);
+  const data = await response.json();
+  const findOl = document.querySelector('.cart__items');
+  findOl.appendChild(createCartItemElement(data));
+  storeCart();
+}
+
+function addNewItem(items) {
+  items.forEach((item) => {
+    const newItem = createProductItemElement(item);
+    const section = document.querySelector('.items');
+    section.appendChild(newItem);
+    const findButton = newItem.querySelector('.item__add');
+    findButton.addEventListener('click', addNewItemToCart);
+  });
+}
+
+async function getApi(userInput) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${userInput}`);
+  const data = await response.json();
+    
+    addNewItem(data.results);
+}
 
 window.onload = () => {
-  getJson('computador');
+  getApi('computador');
+  retrieveCart();
+  emptyButton();
 };
