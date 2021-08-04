e6881cfc8d0f87c1fda5fbcaacfd7a0cb00ef11b
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-08-04 15:34:58 -0300

    Adiciona requisitos 2 e 3

--- script.js
+++ script.js
@@ -24,22 +24,6 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
-
 const addItensToSection = (items) => {
   items.forEach((item) => {
     const ItemElement = createProductItemElement(item); // item == product
@@ -57,6 +41,43 @@ const fetchML = (query) => {
     });
 };
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  event.target.remove();
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function addItem(ItemsIDs) { // Início dos Requisitos 2 e 3
+  return fetch(`https://api.mercadolibre.com/items/${ItemsIDs}`)
+    .then((response) => response.json())
+    .then((data) => data);
+}
+
+const addItemButton = () => {
+  const items = document.querySelector('.items');
+  items.addEventListener('click', async (click) => {
+    if (click.target.className === 'item__add') {
+      const element = click.target.parentElement;
+      const id = getSkuFromProductItem(element);
+      const returnAddItemsFunc = await addItem(id);
+      const li = createCartItemElement(returnAddItemsFunc);
+      document.querySelector('.cart__items').appendChild(li);
+    }
+  });
+}; // Fim dos Requisitos 2 e 3
+
 window.onload = () => {
   fetchML('computador');
+  addItemButton();
 };
