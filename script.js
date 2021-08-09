cff0188714698ee4063485882aa12beb6a085260
Jackson Santana <onjacksm@gmail.com>
2021-08-09 01:35:57 -0300

    Adiciona requisitos 1, 2 e 3

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Global
+const cartItems = '.cart__items';
+// ==========================================================
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,7 +16,8 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+// 1° Desestruturando ID, title, thumbnail que acessar os dados fornecidos pela API
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -24,15 +29,13 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
-  // coloque seu código aqui aaaaqa
+  if (event.target.className === 'cart__item') {
+    event.target.remove();
+  }
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -40,4 +43,51 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+// 2° Fazendo a resuiqição para a API para pegar por ID utilizando async/await
+const pegaComputadorId = async (id) => {
+  const fetchApi = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const jsonObject = await fetchApi.json();
+  return jsonObject;
+};
+
+// Função já existente
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+// Função que adiciona o item ao carrinho
+const bttAdicionarAoCarro = () => {
+  const parent = document.querySelector('.items'); // recupera a classe que contem os 50 computadores
+  parent.addEventListener('click', async (event) => {
+    if (event.target.className === 'item__add') { // Se o local que dispara o evento for igual a classe item__add      
+      const buttonId = getSkuFromProductItem(event.target.parentElement);
+      const buttonData = await pegaComputadorId(buttonId);
+      const createComputer = createCartItemElement(buttonData);
+      document.querySelector(cartItems).appendChild(createComputer);     
+    }
+  });
+};
+
+// 1° Função responsavel por adicionar cada item como filho da section
+const addItensToSection = (items) => {
+  items.forEach((item) => {
+    const itemElement = createProductItemElement(item); // item === produto
+    const section = document.querySelector('.items');
+    section.appendChild(itemElement);
+  });
+};
+
+// 1° Estou fazendo a requisição para a API pegando a informação de todos os computadores
+const buscaML = (query) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+    .then((response) => { // response traz todas a informações
+      response.json().then((data) => {
+        // Utiliza a função addItensToSection para inserir o data.results
+        addItensToSection(data.results);
+      });
+    });
+};
+
+window.onload = () => {
+  buscaML('computador');
+  bttAdicionarAoCarro();
+};
