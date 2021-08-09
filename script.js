7f4f48b51a942d94d5eb10e561e71587943a9dfa
Yuri <yurifranco18@live.com>
2021-08-09 09:55:56 -0300

    foi feito req. 2 com base no req. 1

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function fetchItem(event) {
+  const itemId = event.target.parentNode.firstChild.innerText;
+  fetch(`https://api.mercadolibre.com/items/${itemId}`)
+  .then((response) => {
+    response.json().then((data) => createCartItemElement(data))
+  })
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -9,11 +17,14 @@ function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (element === 'button') {
+    e.addEventListener('click', fetchItem)
+  }
   return e;
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
-  const section = document.createElement('section');
+  const section = do json da função rerequescument.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
@@ -32,13 +43,13 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 //   // coloque seu código aqui
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
 // foi feito o req. 1 utilizando o video que o tio Jack explicando como resolver essa primeira parte.
 const addItensToSection = (items) => {
