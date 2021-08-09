6890f55ece5fed6fa9e2bd7d7cfead108989c77a
Max Salewski <maxsalewski@gmail.com>
2021-08-09 05:05:46 -0300

    Requisitos 3 e 6

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const newOl = document.querySelector('.cart__items');
+const createOl = document.querySelector('.cart__items');
+const cartStr = '.cart__items';
+const btnClear= document.querySelector('.empty-cart');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -30,15 +32,12 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-}
-
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
-  newOl.appendChild(li);
+  createOl.appendChild(li);
 
   return li;
 }
@@ -73,7 +72,7 @@ const getItemId = (element) => {
     response.json()
     .then((data) => {
       const addLi = createCartItemElement(data);
-      newOl.appendChild(addLi);
+      createOl.appendChild(addLi);
     });
   });
 };
@@ -87,9 +86,38 @@ const addButton = () => {
   });
 };
 
+//Requisito 4
+const saveLocalStorage = () => {
+  const olHtml = createOl.innerHTML;
+  localStorage.setItem('lista', olHtml);
+};
+
+function cartItemClickListener(event) {
+  event.target.remove();
+  saveLocalStorage();
+}
+
+const getLocalStorage = () => {
+  const localItems = document.querySelector(cartStr);
+  localItems.innerHTML = localStorage.getItem('lista');
+  const divs = [...createOl.children];
+  divs.forEach((li) => {
+    li.addEventListener('click', cartItemClickListener);
+  });
+};
+
+const clearCart = () => {
+  createOl.innerHTML = '';
+  localStorage.removeItem('lista');
+  document.querySelector('.total-price').innerText = 0;
+};
+
+btnClear.addEventListener('click', clearCart);
+
 window.onload = () => {
   computerFetch();
   addButton();
+  getLocalStorage();
  };
 
  
